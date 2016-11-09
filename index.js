aa00d0432cf8cb0dfd26833df12e9a74e58bf20e
川崎文洋 <fkawasaki@fkawasaki.local>
2016-11-09 15:37:37 +0900

    update push configure.

--- index.js
+++ index.js
@@ -19,7 +19,18 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  push: {
+      android: {
+        senderId: '971068451885',
+        apiKey: 'AIzaSyD-GCZi25gksK9G_royw2BR7OsQBGRskuA'
+      },
+      ios: {
+        pfx: 'hideoutclub.jp.prod.push.p12',
+        bundleId: 'jp.hideoutclub',
+        production: true
+      }
+   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
