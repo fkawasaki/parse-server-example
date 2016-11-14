bcddb1597519296c9cbc6e0b9dde46642f6593c2
川崎文洋 <fkawasaki@dhcp108.gx-m.gaiax.com>
2016-11-14 14:34:29 +0900

    update ' to ".

--- index.js
+++ index.js
@@ -1,54 +1,54 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+var express = require("express");
+var ParseServer = require("parse-server").ParseServer;
+var path = require("path");
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
 if (!databaseUri) {
-  console.log('DATABASE_URI not specified, falling back to localhost.');
+  console.log("DATABASE_URI not specified, falling back to localhost.");
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  clientKey: process.env.CLIENT_KEY || '',
-  restAPIKey: process.env.REST_API_KEY || '',
+  databaseURI: databaseUri || "mongodb://localhost:27017/dev",
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + "/cloud/main.js",
+  appId: process.env.APP_ID || "myAppId",
+  masterKey: process.env.MASTER_KEY || "", //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || "http://localhost:1337/parse",  // Don"t forget to change to https if needed
+  clientKey: process.env.CLIENT_KEY || "",
+  restAPIKey: process.env.REST_API_KEY || "",
   push: {
       android: {
-        senderId: process.env.ANDROID_SENDER_ID || '',
-        apiKey: process.env.ANDROID_API_KEY || ''
+        senderId: process.env.ANDROID_SENDER_ID || "",
+        apiKey: process.env.ANDROID_API_KEY || ""
       },
       ios: {
-        pfx: __dirname + '/' + process.env.PFX_NAME || '',
-        bundleId: process.env.BUNDLE_ID || '',
+        pfx: __dirname + "/" + process.env.PFX_NAME || "",
+        bundleId: process.env.BUNDLE_ID || "",
         production: process.env.IS_PRODUCTION || false
       }
   },
   verifyUserEmails: true,
-  publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
-  appName: process.env.APP_NAME || '',
+  publicServerURL: process.env.SERVER_URL || "http://localhost:1337/parse",
+  appName: process.env.APP_NAME || "",
   emailAdapter: {
-      module: 'parse-server-simple-mailgun-adapter',
+      module: "parse-server-simple-mailgun-adapter",
       options: {
       // The address that your emails come from
-      fromAddress: process.env.MG_FROM_ADDRESS || '',
+      fromAddress: process.env.MG_FROM_ADDRESS || "",
       // Your domain from mailgun.com
-      domain: process.env.MG_DOMAIN_NAME || '',
+      domain: process.env.MG_DOMAIN_NAME || "",
       // Your API key from mailgun.com
-      apiKey: process.env.MG_API_KEY || '',
+      apiKey: process.env.MG_API_KEY || "",
     }
   }
 
 });
 
-console.log('pfx path:');
-console.log(__dirname + '/' + process.env.PFX_NAME );
-console.log('path.join(__dirname, process.env.PFX_NAME) ');
+console.log("pfx path:");
+console.log(__dirname + "/" + process.env.PFX_NAME );
+console.log("path.join(__dirname, process.env.PFX_NAME) ");
 console.log(path.join(__dirname, process.env.PFX_NAME));
 
 
@@ -59,27 +59,27 @@ console.log(path.join(__dirname, process.env.PFX_NAME));
 var app = express();
 
 // Serve static assets from the /public folder
-app.use('/public', express.static(path.join(__dirname, '/public')));
+app.use("/public", express.static(path.join(__dirname, "/public")));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
+var mountPath = process.env.PARSE_MOUNT || "/parse";
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
-app.get('/', function(req, res) {
-  res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
+app.get("/", function(req, res) {
+  res.status(200).send("I dream of being a website.  Please star the parse-server repo on GitHub!");
 });
 
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+app.get("/test", function(req, res) {
+  res.sendFile(path.join(__dirname, "/public/test.html"));
 });
 
 var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
+var httpServer = require("http").createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log("parse-server-example running on port " + port + ".");
 });
 
 // This will enable the Live Query real-time server
