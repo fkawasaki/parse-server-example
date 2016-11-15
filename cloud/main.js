82a8397eb5085448016ffd65c821bbe8b781e5f1
川崎文洋 <fkawasaki@dhcp112.gx-m.gaiax.com>
2016-11-15 17:55:00 +0900

    update cloud code.

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,26 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define("pushTest", function(request,response){
+  var query = new Parse.Query(Parse.User);
+ // var message = request.params.message;
+ var message = 'test message from cloud code.';
+  query.equalTo('objectId', 'UZtbLFXGLn');
+
+  Parse.Push.send({
+    where: query,
+    data : { 
+      alert: message,
+      badge: "Increment",
+      sound: "",
+    }
+    }, {
+    success: function() {
+    //Success
+    },
+    error: function(error) {
+    //Oops
+    }
+  });
+});
