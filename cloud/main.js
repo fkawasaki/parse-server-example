218822f78a49773e22de6d5ed4783a085f97d9d3
川崎文洋 <fkawasaki@fkawasaki.local>
2016-11-16 11:56:12 +0900

    add

--- cloud/main.js
+++ cloud/main.js
@@ -2,6 +2,34 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.afterSave("SendPush", function(request) {
+
+
+  var query = new Parse.Query(Parse.Installation);
+  query.exists("deviceToken");
+
+  // here you can add other conditions e.g. to send a push to sepcific users or channel etc.
+
+  var payload = {
+    alert: "YOUR_MESSAGE"
+      // you can add other stuff here...
+  };
+
+
+  Parse.Push.send({
+      data: payload,
+      where: query
+    }, {
+      useMasterKey: true
+    })
+    .then(function() {
+      response.success("Push Sent!");
+    }, function(error) {
+      response.error("Error while trying to send push " + error.message);
+    });
+});
+
  // vddar message = request.params.message;
 Parse.Cloud.define('push', function(req, res) {
   res.success('called push.');
