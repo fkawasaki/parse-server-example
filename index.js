d59da9321891f538cb2ab7702ef7f508343e7f6b
川崎文洋 <fkawasaki@fkawasaki.local>
2016-11-16 11:10:32 +0900

    remove isProduction.

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var api = new ParseServer({
         pfx: process.env.PFX_NAME || '',
         passphrase: '',
         bundleId: process.env.BUNDLE_ID || '',
-        production: process.env.IS_PRODUCTION || false
+        //production: process.env.IS_PRODUCTION || false
       }
   },
   verifyUserEmails: true,
